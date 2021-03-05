a19cbe82bbb64d16fea6a456ca9358e2abc145f3
Anastasia Gartseva <amgartseva@gmail.com>
2021-03-05 15:35:34 +0300

    feat: Creating load articles-grid | front-page.php, sidebar.php, functions.php, universal-theme.css

--- front-page.php
+++ front-page.php
@@ -130,71 +130,38 @@ Template Post Type: page
     </ul>
     <!-- ./article-list -->
 
-    <ul class="article-grid">
-    <?php		
-        global $post;
+    <div class="main-grid">
+        <ul class="article-grid">
+        <?php		
+            global $post;
 
-        $query = new WP_Query( [
-            'posts_per_page' => 7,
-            'tag' => 'popular'
-        ] );
+            $query = new WP_Query( [
+                'posts_per_page' => 7,
+                'tag' => 'popular'
+            ] );
 
-        if ( $query->have_posts() ) {
-            // Счетчик постов
-            $cnt = 0;
-            while ( $query->have_posts() ) {
-                $query->the_post();
-                $cnt++;
-                switch ($cnt) {
-                    case '1':
-                        ?>
-                            <li class="article-grid-item article-grid-item-1">
-                                <a href="<? the_permalink() ?>" class="article-grid-permalink">
-                                    <span class="category-name"><? $category = get_the_category(); echo $category[0]->name ?></span>
-                                    <h4 class="article-grid-title"><? echo mb_strimwidth(get_the_title(), 0, 50, '...') ?></h4>
-                                    <p class="article-grid-excerpt"><? the_excerpt() ?></p>
-                                    <div class="article-grid-info">
-                                        <div class="author">
-                                            <? $author_id = get_the_author_meta('ID') ?>
-                                            <img src="<?php echo get_avatar_url( $author_id ); ?>" alt="<? the_author() ?>" class="author-avatar">
-                                            <span class="author-name"><strong><? the_author() ?></strong>: <? the_author_meta('description') ?></span>
-                                        </div>
-                                        <div class="comments">
-                                            <!-- <svg width="19" height="15" class="icon comments-icon">
-                                                <use xlink:href=""></use>
-                                            </svg> -->
-                                            <svg width="15" height="14" viewBox="0 0 15 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                <path fill-rule="evenodd" clip-rule="evenodd" d="M11.1346 10.9998V13.9998L8.36064 10.9998H2.25C1.42157 10.9998 0.75 10.3282 0.75 9.49976V1.99976C0.75 1.17133 1.42157 0.499756 2.25 0.499756H12.75C13.5784 0.499756 14.25 1.17133 14.25 1.99976V9.49976C14.25 10.3282 13.5784 10.9998 12.75 10.9998H11.1346Z" fill="#BCBFC2"/>
-                                            </svg>
-                                            <span class="comments-counter"><? comments_number( '0', '1', '%') ?></span>
-                                        </div>
-                                    </div>
-                                </a>
-                            </li>
-                        <?
-                        break;
-                    case '2':
-                        ?>
-                            <li class="article-grid-item article-grid-item-2">
-                                <img src="<?the_post_thumbnail_url()?>" alt="<? the_title() ?>" class="article-grid-thumb">
-                                <a href="<? the_permalink() ?>" class="article-grid-permalink">
-                                    <? $posttags = get_the_tags();
-                                        if ( $posttags ) {
-                                            ?><span class="tag"><?echo $posttags[0]->name . ' ';?></span><?
-                                        }
-                                    ?>
-                                    
-                                    <span class="category-name"><? $category = get_the_category(); echo $category[0]->name ?></span>
-                                    <h4 class="article-grid-title"><? echo mb_strimwidth(get_the_title(), 0, 50, '...') ?></h4>
-                                    <div class="article-grid-info">
-                                        <div class="author">
-                                            <? $author_id = get_the_author_meta('ID') ?>
-                                            <img src="<?php echo get_avatar_url( $author_id ); ?>" alt="<? the_author() ?>" class="author-avatar">
-                                        <div class="author-info">
-                                            <span class="author-name"><strong><? the_author() ?></strong></span>
-                                            <span class="date"><? the_time('j F') ?></span>
+            if ( $query->have_posts() ) {
+                // Счетчик постов
+                $cnt = 0;
+                while ( $query->have_posts() ) {
+                    $query->the_post();
+                    $cnt++;
+                    switch ($cnt) {
+                        case '1':
+                            ?>
+                                <li class="article-grid-item article-grid-item-1">
+                                    <a href="<? the_permalink() ?>" class="article-grid-permalink">
+                                        <span class="category-name"><? $category = get_the_category(); echo $category[0]->name ?></span>
+                                        <h4 class="article-grid-title"><? echo mb_strimwidth(get_the_title(), 0, 50, '...') ?></h4>
+                                        <p class="article-grid-excerpt"><? the_excerpt() ?></p>
+                                        <div class="article-grid-info">
+                                            <div class="author">
+                                                <? $author_id = get_the_author_meta('ID') ?>
+                                                <img src="<?php echo get_avatar_url( $author_id ); ?>" alt="<? the_author() ?>" class="author-avatar">
+                                                <span class="author-name"><strong><? the_author() ?></strong>: <? the_author_meta('description') ?></span>
+                                            </div>
                                             <div class="comments">
-                                                <!-- <svg width="19" height="15" fill='#fff' class="icon comments-icon">
+                                                <!-- <svg width="19" height="15" class="icon comments-icon">
                                                     <use xlink:href=""></use>
                                                 </svg> -->
                                                 <svg width="15" height="14" viewBox="0 0 15 14" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -202,48 +169,85 @@ Template Post Type: page
                                                 </svg>
                                                 <span class="comments-counter"><? comments_number( '0', '1', '%') ?></span>
                                             </div>
-                                            <div class="likes">
-                                                <img src="<? echo get_template_directory_uri() ?>/assets/images/heart.svg" alt="icon: like" class="likes-icon">
-                                                <span class="likes-counter"><? comments_number( '0', '1', '%') ?></span>
+                                        </div>
+                                    </a>
+                                </li>
+                            <?
+                            break;
+                        case '2':
+                            ?>
+                                <li class="article-grid-item article-grid-item-2">
+                                    <img src="<?the_post_thumbnail_url()?>" alt="<? the_title() ?>" class="article-grid-thumb">
+                                    <a href="<? the_permalink() ?>" class="article-grid-permalink">
+                                        <? $posttags = get_the_tags();
+                                            if ( $posttags ) {
+                                                ?><span class="tag"><?echo $posttags[0]->name . ' ';?></span><?
+                                            }
+                                        ?>
+                                        
+                                        <span class="category-name"><? $category = get_the_category(); echo $category[0]->name ?></span>
+                                        <h4 class="article-grid-title"><? echo mb_strimwidth(get_the_title(), 0, 50, '...') ?></h4>
+                                        <div class="article-grid-info">
+                                            <div class="author">
+                                                <? $author_id = get_the_author_meta('ID') ?>
+                                                <img src="<?php echo get_avatar_url( $author_id ); ?>" alt="<? the_author() ?>" class="author-avatar">
+                                            <div class="author-info">
+                                                <span class="author-name"><strong><? the_author() ?></strong></span>
+                                                <span class="date"><? the_time('j F') ?></span>
+                                                <div class="comments">
+                                                    <!-- <svg width="19" height="15" fill='#fff' class="icon comments-icon">
+                                                        <use xlink:href=""></use>
+                                                    </svg> -->
+                                                    <svg width="15" height="14" viewBox="0 0 15 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                        <path fill-rule="evenodd" clip-rule="evenodd" d="M11.1346 10.9998V13.9998L8.36064 10.9998H2.25C1.42157 10.9998 0.75 10.3282 0.75 9.49976V1.99976C0.75 1.17133 1.42157 0.499756 2.25 0.499756H12.75C13.5784 0.499756 14.25 1.17133 14.25 1.99976V9.49976C14.25 10.3282 13.5784 10.9998 12.75 10.9998H11.1346Z" fill="#BCBFC2"/>
+                                                    </svg>
+                                                    <span class="comments-counter likes-counter"><? comments_number( '0', '1', '%') ?></span>
+                                                </div>
+                                                <div class="likes">
+                                                    <img src="<? echo get_template_directory_uri() ?>/assets/images/heart.svg" alt="icon: like" class="likes-icon">
+                                                    <span class="likes-counter"><? comments_number( '0', '1', '%') ?></span>
+                                                </div>
+                                            </div>
+                                            <!-- /.author-info -->
                                             </div>
                                         </div>
-                                        <!-- /.author-info -->
-                                        </div>
-                                    </div>
-                                </a>
-                            </li>
-                        <?
-                        break;
-                    case '3':
-                        ?>
-                        <li class="article-grid-item article-grid-item-3">
-                            <a href="<? the_permalink() ?>" class="article-grid-permalink">
-                                <img src="<?the_post_thumbnail_url()?>" alt="<? the_title() ?>" class="article-thumb">
-                                <h4 class="article-grid-title"><? echo mb_strimwidth(get_the_title(), 0, 40, '...') ?></h4>
-                            </a>
-                        </li>
-                        <?
-                        break;
-                    default:
-                        ?>
-                            <li class="article-grid-item article-grid-item-default">
+                                    </a>
+                                </li>
+                            <?
+                            break;
+                        case '3':
+                            ?>
+                            <li class="article-grid-item article-grid-item-3">
                                 <a href="<? the_permalink() ?>" class="article-grid-permalink">
-                                    <h4 class="article-grid-title"><? echo mb_strimwidth(get_the_title(), 0, 20, '...') ?></h4>
-                                    <p class="article-grid-excerpt"><? echo mb_strimwidth(get_the_excerpt(), 0, 110, '...') ?></p>
-                                    <span class="article-date"><? the_time('j F') ?></span>
+                                    <img src="<?the_post_thumbnail_url()?>" alt="<? the_title() ?>" class="article-thumb">
+                                    <h4 class="article-grid-title"><? echo mb_strimwidth(get_the_title(), 0, 40, '...') ?></h4>
                                 </a>
                             </li>
-                        <?
-                        break;
+                            <?
+                            break;
+                        default:
+                            ?>
+                                <li class="article-grid-item article-grid-item-default">
+                                    <a href="<? the_permalink() ?>" class="article-grid-permalink">
+                                        <h4 class="article-grid-title"><? echo mb_strimwidth(get_the_title(), 0, 20, '...') ?></h4>
+                                        <p class="article-grid-excerpt"><? echo mb_strimwidth(get_the_excerpt(), 0, 110, '...') ?></p>
+                                        <span class="article-date"><? the_time('j F') ?></span>
+                                    </a>
+                                </li>
+                            <?
+                            break;
+                    }
                 }
+            } else {
+                // Постов не найдено
             }
-        } else {
-            // Постов не найдено
-        }
 
-        wp_reset_postdata(); // Сбрасываем $post
-    ?>
-    </ul>
-    <!-- /.article-grid -->
+            wp_reset_postdata(); // Сбрасываем $post
+        ?>
+        </ul>
+        <!-- /.article-grid -->
+
+        <? get_sidebar() ?>
+    </div>
 </div>
-<? get_footer(); ?>
+<? get_footer();
