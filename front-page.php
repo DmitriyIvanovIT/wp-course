2d35ffe9338cfb709a9422ad5ec670463e42792f
Anastasia Gartseva <amgartseva@gmail.com>
2021-03-09 14:12:36 +0300

    feat: Creating post investigation and digest | front-page.php, universal-theme.css

--- front-page.php
+++ front-page.php
@@ -101,7 +101,8 @@ Template Post Type: page
 
             $myposts = get_posts([ 
                 'numberposts' => 4,
-                'category_name' => 'articles'
+                'category_name' => 'articles',
+                'category__not_in' => array(24, 26, 25, 27, 28)
             ]);
 
             // Проверка постов
@@ -137,7 +138,8 @@ Template Post Type: page
 
             $query = new WP_Query( [
                 'posts_per_page' => 7,
-                'tag' => 'popular'
+                'tag' => 'popular',
+                'category__not_in' => array(24, 26, 25, 27, 28)
             ] );
 
             if ( $query->have_posts() ) {
@@ -257,5 +259,100 @@ Template Post Type: page
 
         <? get_sidebar() ?>
     </div>
+
+    <? 
+        $myposts = get_posts([ 
+            'numberposts' => 1,
+            'category_name' => 'investigation' 
+        ]); 
+            // Проверка постов
+            if( $myposts ){
+                // Если есть, запускаем цикл
+                foreach( $myposts as $post ){
+                    setup_postdata( $post );
+                    ?>
+                    <section class="investigation" style="background: linear-gradient(0deg, rgba(64, 48, 61, 0.65), rgba(64, 48, 61, 0.65)), url(<?the_post_thumbnail_url()?>) no-repeat center center">
+                        <div class="container">
+                            <h2 class="investigation-title"><? the_title() ?></h2>
+                            <a href="<? the_permalink() ?>" class="more">Читать статью</a>
+                        </div>
+                    </section>
+    <?php 
+            }
+        } else {
+            ?><p>
+                    Постов нет
+            </p> <?
+        }
+
+        wp_reset_postdata(); // Сбрасываем $post
+    ?>
+
+<div class="digest-wrapper">
+    <ul class="digest">
+        <? 
+            $myposts = get_posts([ 
+                'numberposts' => 6,
+                'category_name' => 'news, opinions, hot, collections' 
+            ]); 
+                // Проверка постов
+                if( $myposts ){
+                    // Если есть, запускаем цикл
+                    foreach( $myposts as $post ){
+                        setup_postdata( $post );
+                        ?>
+                            <li class="digest-item">
+                                <a href="<? the_permalink() ?>" class="digest-item-permalink">
+                                    <img src="<?the_post_thumbnail_url()?>" alt="<? the_title() ?>" class="digest-thumb">
+                                </a>
+                                <div class="digest-info">
+                                    <button class="bookmark">
+                                    <!-- <svg width="14" height="18" class="icon icon-bookmark">
+                                        <use xlink:href=""></use>
+                                    </svg> -->
+                                    <svg width="14" height="18" viewBox="0 0 14 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path fill-rule="evenodd" clip-rule="evenodd" d="M2 0H12C13.1046 0 14 0.89543 14 2V18L7.09495 13L0 18V2C0 0.89543 0.89543 0 2 0Z" fill="#BCBFC2"/>
+                                    </svg>
+
+                                    </button>
+                                    <? $category = get_the_category(); ?>
+                                    <a href="<? echo $category[0]->name ?>" class="category-link"><? echo $category[0]->name ?></a>
+                                    <a href="#" class="digest-item-permalink">
+                                    <h3 class="digest-title"><? echo mb_strimwidth(get_the_title(), 0, 50, '...') ?></h3>
+                                    </a>
+                                    <p class="digest-excerpt"><? echo mb_strimwidth(get_the_excerpt(), 0, 200, '...') ?></p>
+                                    <div class="digest-footer">
+                                    <span class="digest-date"><? the_time('j F') ?></span>
+                                    <div class="comments digest-comments">
+                                        <!-- <svg width="19" height="15" class="icon comments-icon">
+                                        <use xlink:href=""></use>
+                                        </svg> -->
+                                        <svg width="15" height="14" viewBox="0 0 15 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path fill-rule="evenodd" clip-rule="evenodd" d="M11.1346 10.9998V13.9998L8.36064 10.9998H2.25C1.42157 10.9998 0.75 10.3282 0.75 9.49976V1.99976C0.75 1.17133 1.42157 0.499756 2.25 0.499756H12.75C13.5784 0.499756 14.25 1.17133 14.25 1.99976V9.49976C14.25 10.3282 13.5784 10.9998 12.75 10.9998H11.1346Z" fill="#BCBFC2"/>
+                                        </svg>
+                                        <span class="comments-counter"><? comments_number( '0', '1', '%') ?></span>
+                                    </div>
+                                    <div class="likes digest-likes">
+                                        <img src="<? echo get_template_directory_uri() ?>/assets/images/heart-grey.svg" alt="icon: like" class="likes-icon">
+                                        <span class="likes-counter"><? comments_number( '0', '1', '%') ?></span>
+                                    </div>
+                                    </div>
+                                    <!-- /.digest-footer -->
+                                </div>
+                                <!-- /.digest-info -->
+                            </li>
+        <?php 
+                }
+            } else {
+                ?><p>
+                        Постов нет
+                </p> <?
+            }
+
+            wp_reset_postdata(); // Сбрасываем $post
+        ?>
+        
+    </ul>
+</div>
 </div>
 <? get_footer();
